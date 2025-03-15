b04ad61e246618c78698da7c908838560bde48ac
atul-mps <atul.dhadse@highwirepress.com>
2025-03-15 15:26:27 +0530

    initial commit

---
+++ src/main/java/com/blah/selectivejsonfetch/SelectiveJsonFetchApplication.java
@@ -0,0 +1,13 @@
+package com.blah.selectivejsonfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectiveJsonFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectiveJsonFetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blah/selective_json_fetch/SelectiveJsonFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blah.selective_json_fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectiveJsonFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
